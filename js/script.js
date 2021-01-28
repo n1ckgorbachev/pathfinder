3305298eec709ceda012112bc61283bc791c8d02
Nikolay Gorbachev <n1kko.gorbachevv@gmail.com>
2021-01-28 18:52:51 +0300

    создал js файл с вводом

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+//Получение значений из html
+function getValue(){
+    let d4 = document.getElementById('d4__value').value;
+    let d6 = document.getElementById('d6__value').value;
+    let d8 = document.getElementById('d8__value').value;
+    let d10 = document.getElementById('d10__value').value;
+    let d12 = document.getElementById('d12__value').value;
+
+    
+}
+
+
+
+let result = 20;
+    result = 20 + "%";
+    document.getElementById('result').innerHTML = result;
